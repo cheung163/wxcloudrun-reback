e202e27cac5db0944f0bf78edd6eefc6b8586d02
cheung163 <cheungjingyu@163.com>
2022-08-10 22:43:51 +0800

    dome

--- src/main/java/com/tencent/wxcloudrun/handler/CommonHandler.java
+++ src/main/java/com/tencent/wxcloudrun/handler/CommonHandler.java
@@ -9,6 +9,7 @@ import com.tencent.wxcloudrun.vo.TextVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.Map;
 
 @Component
@@ -21,23 +22,33 @@ public class CommonHandler {
         String msg;
         if (form.getContent().contains("-")) {
             String[] split = form.getContent().split("-");
-            HomeProduce homeProduce = new HomeProduce();
-            homeProduce.setReduceId(IdUtil.getSnowflakeNextIdStr());
-            homeProduce.setProduce(split[0]);
-            homeProduce.setAddress(split[1]);
-            homeProduceService.save(homeProduce);
+
+            QueryWrapper<HomeProduce> qw = new QueryWrapper<>();
+            qw.eq("produce", split[0]);
+            HomeProduce one = homeProduceService.getOne(qw, false);
+            if (one == null) {
+                HomeProduce homeProduce = new HomeProduce();
+                homeProduce.setProduce(split[0]);
+                homeProduce.setReduceId(IdUtil.getSnowflakeNextIdStr());
+                homeProduce.setAddress(split[1]);
+                homeProduceService.saveOrUpdate(homeProduce);
+            } else {
+                one.setAddress(split[1]);
+                one.setCreatetime(new Date());
+                homeProduceService.updateById(one);
+            }
+
             msg = "已经为您保存好了，我的小主";
-        }
-        {
+        } else {
             QueryWrapper<HomeProduce> qw = new QueryWrapper<>();
             qw.eq("produce", form.getContent());
             HomeProduce one = homeProduceService.getOne(qw, false);
-
             if (one != null) {
                 msg = "没记得您告诉过我呐，请小主再好好想想！";
+            } else {
+                msg = "小主，您的" + form.getContent() + "放在" + one.getAddress() + "里";
             }
-            msg = "小主，您的" + form.getContent() + "放在" + one.getAddress() + "里";
-            return TextVo.create(form, msg);
         }
+        return TextVo.create(form, msg);
     }
 }
