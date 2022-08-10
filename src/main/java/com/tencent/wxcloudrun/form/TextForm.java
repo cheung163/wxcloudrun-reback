9e5aa605fa9eecc66f4247ade1fa40abc934533f
cheung163 <cheungjingyu@163.com>
2022-08-10 22:59:55 +0800

    dome

--- src/main/java/com/tencent/wxcloudrun/form/TextForm.java
+++ src/main/java/com/tencent/wxcloudrun/form/TextForm.java
@@ -1,16 +1,21 @@
 package com.tencent.wxcloudrun.form;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class TextForm {
 
+    @JsonProperty("ToUserName")
     private String ToUserName;
+    @JsonProperty("FromUserName")
     private String FromUserName;
+    @JsonProperty("CreateTime")
     private int CreateTime;
+    @JsonProperty("MsgType")
     private String MsgType;
+    @JsonProperty("Content")
     private String Content;
+    @JsonProperty("MsgId")
     private long MsgId;
-
-
 }
