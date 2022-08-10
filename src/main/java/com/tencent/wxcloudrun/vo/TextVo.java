0c3da53ffb2ee6466e5dde31b04c48c09bf55cf0
cheung163 <cheungjingyu@163.com>
2022-08-10 23:28:56 +0800

    TextVo

--- src/main/java/com/tencent/wxcloudrun/vo/TextVo.java
+++ src/main/java/com/tencent/wxcloudrun/vo/TextVo.java
@@ -1,6 +1,7 @@
 package com.tencent.wxcloudrun.vo;
 
 import cn.hutool.core.date.DateUtil;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.tencent.wxcloudrun.form.TextForm;
 import lombok.Builder;
 import lombok.Data;
@@ -9,10 +10,15 @@ import lombok.RequiredArgsConstructor;
 @Data
 @RequiredArgsConstructor
 public class TextVo {
+    @JsonProperty("ToUserName")
     private final String ToUserName;
+    @JsonProperty("FromUserName")
     private final String FromUserName;
+    @JsonProperty("CreateTime")
     private long CreateTime = DateUtil.current() / 1000;
+    @JsonProperty("MsgType")
     private final String MsgType = "text";
+    @JsonProperty("Content")
     private final String Content;
 
 
